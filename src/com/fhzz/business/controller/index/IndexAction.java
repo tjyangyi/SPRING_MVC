f88423e681130f88564c44aad0ac965a31cc67dd
YangYi <368315960@qq.com>
2018-02-04 23:52:54 +0800

    加入了界面和后台获取security中的user

--- src/com/fhzz/business/controller/index/IndexAction.java
+++ src/com/fhzz/business/controller/index/IndexAction.java
@@ -5,9 +5,12 @@ package com.fhzz.business.controller.index;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.fhzz.business.entity.sys.SysUsers;
 import com.fhzz.core.controller.BaseAction;
 
 /**
@@ -20,6 +23,14 @@ public class IndexAction extends BaseAction {
 
 	@RequestMapping("toIndex")
 	public String toLogin() {
+		UserDetails user = (UserDetails) SecurityContextHolder.getContext()
+				.getAuthentication().getPrincipal();
+		logger.info(user);
+		logger.info(user.getUsername());
+		SysUsers users = (SysUsers) SecurityContextHolder.getContext()
+				.getAuthentication().getPrincipal();
+		logger.info(users);
+		logger.info(users.getName());
 		return "index/index";
 	}
 
